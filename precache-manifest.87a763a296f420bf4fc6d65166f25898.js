e8e463827907485eb200e8ffd90d86446d8b786e
Pranav Gupta <guptapranav4299@gmail.com>
2021-02-15 23:51:30 +0530

    Updates

---
+++ precache-manifest.87a763a296f420bf4fc6d65166f25898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b295f023b25bc34d66f4563f10701ae7",
+    "url": "/todoist-clone/index.html"
+  },
+  {
+    "revision": "26e9cdd68dc1eb39bd75",
+    "url": "/todoist-clone/static/css/main.67c65cf9.chunk.css"
+  },
+  {
+    "revision": "8a6ae31562fcf21955c0",
+    "url": "/todoist-clone/static/js/2.513b398d.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/todoist-clone/static/js/2.513b398d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e9cdd68dc1eb39bd75",
+    "url": "/todoist-clone/static/js/main.3dbc1c86.chunk.js"
+  },
+  {
+    "revision": "d55ebb1c13e5cf871224",
+    "url": "/todoist-clone/static/js/runtime-main.d80ab701.js"
+  }
+]);
